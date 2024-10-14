393ded9f6ca397f3c146eba9e489a67c6d90df73
codewithswatiee <verma.s@somaiya.edu>
2024-10-14 18:42:39 +0530

    "Added published_date field to Book class and updated related code, added HTTPException for 404 error, and added new endpoint for books by publish date."

--- book2.py
+++ book2.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
 app = FastAPI()
 
@@ -10,13 +10,15 @@ class Book:
     author: str
     rating: int
     description: str
+    published_date: int
 
-    def __init__(self, id, title, author, rating, description):
+    def __init__(self, id, title, author, rating, published_date, description):
         self.author = author
         self.description = description
         self.title = title
         self.rating = rating
         self.id = id
+        self.published_date = published_date
 
 
 class BookRequest(BaseModel):
@@ -25,6 +27,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     rating: int = Field(gt=0, lt=6)
     description: str = Field(min_length=1, max_length=100)
+    published_date: int = Field(gt=1999, lt=2031)
 
     class Config: 
         schema_extra = {
@@ -32,19 +35,20 @@ class BookRequest(BaseModel):
                 "title": "a new book",
                 "author": "John Doe",
                 "rating" : 5,
-                "description" : "A new description"
+                "description" : "A new description",
+                "published_date" : 2020
             }
         }
 
 
 
 BOOKS = [
-    Book(1, "Book 1", "Author 1", 5, "This is book 1"),
-    Book(2, "Book 2", "Author 2", 5, "This is book 2"),
-    Book(3, "Book 3", "Author 3", 5, "This is book 3"),
-    Book(4, "Book 4", "Author 4", 5, "This is book 4"),
-    Book(5, "Book 4", "Author 4", 5, "This is book 5"),
-    Book(6, "Book 4", "Author 4", 5, "This is book 6"),
+    Book(1, "Book 1", "Author 1", 5, "This is book 1", 2011),
+    Book(2, "Book 2", "Author 2", 5, "This is book 2", 2023),
+    Book(3, "Book 3", "Author 3", 5, "This is book 3", 2012),
+    Book(4, "Book 4", "Author 4", 5, "This is book 4", 2034),
+    Book(5, "Book 4", "Author 4", 5, "This is book 5", 2034),
+    Book(6, "Book 4", "Author 4", 5, "This is book 6", 2001),
 ]
 
 @app.get("/")
@@ -53,18 +57,30 @@ async def root():
 
 
 @app.get("/books/{book_id}")
-async def read_book(book_id: int):
+async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
+    raise HTTPException(status_code=404, detail="Item not found")
+
         
 @app.get("/books/")
-async def read_book_by_rating(rating: int):
+async def read_book_by_rating(rating: int = Query(gt=0, lt=6)):
     book_to_return = []
     for book in BOOKS:
         if book.rating == rating:
             book_to_return.append(book)
     return book_to_return
+
+
+
+@app.get("/books/publish/")
+async def books_by_publish(publish_date: int = Query(gt=1999, lt=2031)):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get(publish_date) == publish_date:
+            books_to_return.append(book)
+    return books_to_return
 
 
 @app.post("/create-book")
@@ -91,7 +107,7 @@ async def update_book(book: BookRequest):
             BOOKS[i] = book
 
 @app.delete("/book/{book_id}")
-async def delete_book(book_id: int):
+async def delete_book(book_id: int = Path(gt=0)):
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             del BOOKS[i]
